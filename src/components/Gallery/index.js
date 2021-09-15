c94bcdcfb00cebe5d5c89ee6ec89d174b282334e
dpbam <derekphtpt@gmail.com>
2021-09-15 14:23:36 -0600

    20.3.5 FINALLY working

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,26 +1,17 @@
 import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
-import photo from "../../assets/small/commercial/0.jpg";
+import PhotoList from "../PhotoList";
 
-const Gallery = (props) => {
-  const currentCategory = {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  };
+function Gallery(props) {
+  const { name, description } = props.currentCategory;
+
   return (
     <section>
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.description}</p>
-      <div>
-        <img
-          src={photo}
-          alt="Commercial Example"
-          className="img-thumbnail mx-1"
-        />
-      </div>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+      <p>{description}</p>
+      <PhotoList category={props.currentCategory.name} />
     </section>
   );
-};
+}
 
 export default Gallery;
